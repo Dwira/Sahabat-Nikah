38599aa7ea383b968f482fef4125d54aea4b5a32
Fatih Kalifa <fatihkalifa@gmail.com>
2012-07-07 13:24:21 +0700

    Base Fix
    1. Tambahan Base URL supaya gampang load CSS / file lain
    2. Folder Theme ditambahkan, untuk menyimpan file-file layout, misal css
    3. Alamat CSS pada Header diperbaiki

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,7 +1,7 @@
 <html>
 <head>
 <title>Sahabat Nikah - <?= $title ?></title>
-<link rel="stylesheet" type="text/css" href="style.css" />
+<link rel="stylesheet" type="text/css" href="<?= base_url() ?>theme/style/style.css" />
 </head>
 <body>
 <div id="header">

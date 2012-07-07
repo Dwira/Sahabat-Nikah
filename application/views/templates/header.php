25a6552d074aa310c725b82c672caa9de7c94afe
Fatih Kalifa <fatihkalifa@gmail.com>
2012-07-07 13:17:59 +0700

    Update Content 1

---
+++ application/views/templates/header.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>Sahabat Nikah - <?= $title ?></title>
+<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+<div id="header">
+	<div id="logo">
+	</div>
+	<?php include("navigation.php"); ?>
+</div>
---
+++ application/views/templates/navigation.php
@@ -0,0 +1,10 @@
+<div id="menu">
+	<ul id="main">
+		<li><a href="index.html">HOME</a></li>
+		<li><a href="about.html">ABOUT US</a></li>
+		<li><a href="paket.html">PACKAGE</a></li>
+		<li><a href="galeri.html">GALLERY</a></li>
+		<li><a href="contact.html">CONTACT US</a></li>
+		<div class="clearfix"></div>
+	</ul>
+</div>

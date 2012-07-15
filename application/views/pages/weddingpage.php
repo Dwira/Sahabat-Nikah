7986c2f5736508de166e2087c69cc63b0cf92fdb
dwira <dwirakwidodo@gmail.com>
2012-07-15 20:22:40 +0700

    weedingpage v0.01+list broadcast app v0.04+icon

--- application/views/pages/weddingpage.php
+++ application/views/pages/weddingpage.php
@@ -1,11 +1,27 @@
-<div id="main-content">
-	<div id="pic-column">
-		<img src="1.jpg" width="400" height="300" />
+<div class="page">
+	<div id="main-content">
+		<div id="pic-column">
+			<img src="1.jpg" width="400" height="300" />
+		</div>
+		<div id="about-coloumn">
+			<h1 class="text">Wedding Proxy</h1>
+			<h2 class="text">Gunawan Sasongko & Rizki Amalia</h2>
+			<h3 class="text">Sunday, 15 July 2012</br>Tempat Mana pun lah</h3>
+			<div id="button-watch" ><a href="#"><img src="<?=base_url();?>theme/img/watch.jpg" height ="100px" width="100px"/></div> 
+		</div>
 	</div>
-	<div id="about-coloumn">
-		<h1 class="text">Wedding Proxy</h1>
-		<h2 class="text">Gunawan Sasongko & Rizki Amalia</h2>
-		<h3 class="text">Sunday, 15 July 2012</br>Tempat Mana pun lah</h3>
-		<div id="button-watch" ><a href="#"><img src="<?=base_url();?>theme/img/watch.jpg" height ="50px" width="200px"/></div>
+</div>
+<div class="page">
+	<div id="content-left">
+		INVITATION
 	</div>
+	<div id="content-right">
+		<img src="<?=base_url();?>theme/img/map.jpg" height ="300px" width="400px"/>
+	</div>
+</div>
+<div class="page">
+	<h1>cerita kami</h1>
+</div>
+<div class="page">
+	<h1>galery photo</h1>
 </div>

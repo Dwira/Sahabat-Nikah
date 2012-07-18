9a22316bbee2f250bb34c1fb6def3b42f71ceca7
dwira <dwirakwidodo@gmail.com>
2012-07-18 07:08:21 +0700

    weddingpage v0.02
    kurang galery

--- application/views/pages/weddingpage.php
+++ application/views/pages/weddingpage.php
@@ -1,27 +1,55 @@
 <div class="page">
 	<div id="main-content">
 		<div id="pic-column">
-			<img src="1.jpg" width="400" height="300" />
+			<img src="<?=base_url();?>theme/img/mount.jpg" width="400" height="300" />
 		</div>
 		<div id="about-coloumn">
 			<h1 class="text">Wedding Proxy</h1>
 			<h2 class="text">Gunawan Sasongko & Rizki Amalia</h2>
 			<h3 class="text">Sunday, 15 July 2012</br>Tempat Mana pun lah</h3>
-			<div id="button-watch" ><a href="#"><img src="<?=base_url();?>theme/img/watch.jpg" height ="100px" width="100px"/></div> 
+			<div id="button-watch" ><a href="#"><img src="<?=base_url();?>theme/img/watch.jpg" height ="100px" width="100px"/></a></div> 
 		</div>
 	</div>
 </div>
+<div class="clearfix"></div>
 <div class="page">
-	<div id="content-left">
-		INVITATION
-	</div>
-	<div id="content-right">
-		<img src="<?=base_url();?>theme/img/map.jpg" height ="300px" width="400px"/>
+	<div class="content-wedding">
+		<div id="content-left">
+			INVITATION
+		</div>
+		<div id="content-right">
+			<img src="<?=base_url();?>theme/img/map.jpg" height ="300px" width="420px"/>
+		</div>
 	</div>
 </div>
+<div class="clearfix"></div>
 <div class="page">
-	<h1>cerita kami</h1>
+	<div class="content-wedding">
+	<h1>CERITA KAMI</h1>
+	<p>
+		Pertama kali kami bertemu dikenalkan oleh teman kami kurang lebih 7 tahun yang lalu.
+		Berbagai macam suka dan duka kami lewati bersama, bukan tidak pernah kami bertengkar satu sama lain, bahkan di awal kebersamaan kami itu cukup sering,hehehe....
+
+		Perlahan kami bersama2 melalui semua proses kehidupan dengan doa dan syukur dgn air mata dan tawa, semua ini baik adanya.
+
+		Semakin lama semakin kami mengenal masing2 dan kami akan terus mengasihi satu sama lain, kami belajar mengasihi tanpa syarat satu sama lain. Kami mau katakan disini bahwa cinta murni itu ada dan cinta itu diatas segalanya. Kasih menutupi banyak sekali pelanggaran.
+
+		Tujuh tahun berlalu kami tidak pernah menyesal sudah dipertemukan. Perjalanan masih panjang untu dilewati. kami siap bersama2 dgn cinta dan TUHAN disisi kami. A new life is yet to come.
+		Cinta sejati adalah menerima apa adanya yang terjadi saat ini, yang sudah terjadi, yang akan terjadi, dan yang tidak akan pernah terjadi...
+
+		 
+
+		Tapi kami sadar perlahan bahwa semua itu mendatangkan kebaikan bagi kami, jatuh bangun kami bersama adalah suatu proses cinta tanpa syarat yg kami coba bangun dalam hubungan kami berdua. cinta tanpa syarat adalah cinta yg murni.
+
+
+						Kami percaya  dengan mensyukuri apa yg ada dan Hidup Adalah anugrah..Tuhan tak pernah salah
+
+
+
+		SHMILY</p>
+	</div>
 </div>
+<div class="clearfix"></div>
 <div class="page">
-	<h1>galery photo</h1>
+	<h1>GALERY</h1>
 </div>

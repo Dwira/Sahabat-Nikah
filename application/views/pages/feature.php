fe4f6b75e07f58c69ab316fbfe4cc91eb197f508
dwira <dwirakwidodo@gmail.com>
2012-07-25 13:18:52 +0700

    feature 9 fitur kurang icon

--- application/views/pages/feature.php
+++ application/views/pages/feature.php
@@ -2,6 +2,7 @@
 	
 		<div class="fitur-coloumn">
 			<div class="fitur-icon">
+				<img src="<?=base_url();?>theme/img/.jpg" width="80" height="100"/>
 			</div>
 			<div class="fitur-text">
 				<h4>Live Streaming</h4>
@@ -9,28 +10,109 @@
 			</div>
 		</div>
 		<div class="fitur-coloumn">
+			<div class="fitur-icon">
+				<img src="<?=base_url();?>theme/img/.jpg" width="80" height="100"/>
+			</div>
+			<div class="fitur-text">
+				<h4>Live Interaction</h4>
+				<p>Dengan memberikan komen pada video dan foto, tamu dapat berinteraksi dengan tamu lainnya yang melihat video secara langsung</p>
+			</div>
 		</div>
 		<div class="fitur-coloumn">
+			<div class="fitur-icon">
+				<img src="<?=base_url();?>theme/img/.jpg" width="80" height="100"/>
+			</div>
+			<div class="fitur-text">
+				<h4>Wedding Page</h4>
+				<p>Di halaman buku ini pasangan pengantin dapat mengabadikan kisah kasih dan foto-foto romantis mereka</p>
+			</div>
 		</div>
-
+		<div class="clearfix"></div>
+		
 		<div class="fitur-coloumn">
+			<div class="fitur-icon">
+				<img src="<?=base_url();?>theme/img/.jpg" width="80" height="100"/>
+			</div>
+			<div class="fitur-text">
+				<h4>Buku Tamu</h4>
+				<p>Pengantin akan mengetahui siapa saja tamu yang melihat Wedding Page mereka serta kesan-kesan yang mereka sampaikan untuk pengantin</p>
+			</div>
 		</div>
 		<div class="fitur-coloumn">
+			<div class="fitur-icon">
+				<img src="<?=base_url();?>theme/img/.jpg" width="80" height="100"/>
+			</div>
+			<div class="fitur-text">
+				<h4>Galery Foto</h4>
+				<p>Berisi foto-foto susasana acara pernikahan yang di up-to-date diunggah langsung pada saat acara</p>
+			</div>
 		</div>
 		<div class="fitur-coloumn">
+			<div class="fitur-icon">
+				<img src="<?=base_url();?>theme/img/.jpg" width="80" height="100"/>
+			</div>
+			<div class="fitur-text">
+				<h4>Video Comment</h4>
+				<p>tamu undangan yang berhalangan untuk datang pada saat acara dapat merekam pesan berbentuk video yang ditujukan pada pengantin</p>
+			</div>
 		</div>
-
+		<div class="clearfix"></div>
+		
 		<div class="fitur-coloumn">
+			<div class="fitur-icon">
+				<img src="<?=base_url();?>theme/img/.jpg" width="80" height="100"/>
+			</div>
+			<div class="fitur-text">
+				<h4>Connect Facebook</h4>
+				<p>kamu dapat memberikan komen dengan akun FB </p>
+			</div>
 		</div>
 		<div class="fitur-coloumn">
+			<div class="fitur-icon">
+				<img src="<?=base_url();?>theme/img/.jpg" width="80" height="100"/>
+			</div>
+			<div class="fitur-text">
+				<h4>Doorprize</h4>
+				<p>tamu yang telah mengisi di buku tamu, akan mendapatkan doorprize bila beruntung undian</p>
+			</div>
 		</div>
 		<div class="fitur-coloumn">
+			<div class="fitur-icon">
+				<img src="<?=base_url();?>theme/img/.jpg" width="80" height="100"/>
+			</div>
+			<div class="fitur-text">
+				<h4>Record via smartphone</h4>
+				<p>kamu dapat merekam video pernikahan dengan mudah dengan menggunakan smartphone</p>
+			</div>
 		</div>
-
+		<div class="clearfix"></div>
+		
 		<div class="fitur-coloumn">
+			<div class="fitur-icon">
+				<img src="<?=base_url();?>theme/img/.jpg" width="80" height="100"/>
+			</div>
+			<div class="fitur-text">
+				<h4></h4>
+				<p></p>
+			</div>
 		</div>
 		<div class="fitur-coloumn">
+			<div class="fitur-icon">
+				<img src="<?=base_url();?>theme/img/.jpg" width="80" height="100"/>
+			</div>
+			<div class="fitur-text">
+				<h4></h4>
+				<p></p>
+			</div>
 		</div>
 		<div class="fitur-coloumn">
+			<div class="fitur-icon">
+				<img src="<?=base_url();?>theme/img/.jpg" width="80" height="100"/>
+			</div>
+			<div class="fitur-text">
+				<h4></h4>
+				<p></p>
+			</div>
 		</div>
+		<div class="clearfix"></div>
 </ul>

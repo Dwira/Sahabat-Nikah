7ef8da5629e8b7ebdeac9161b3b1fab09282abac
dwira <dwirakwidodo@gmail.com>
2012-07-25 12:22:41 +0700

    style

--- application/views/pages/feature.php
+++ application/views/pages/feature.php
@@ -1,18 +1,36 @@
 <ul id= "fitur-content">
 	
-		<div class="fitur-coloumn"> </div>
-		<div class="fitur-coloumn"> </div>
-		<div class="fitur-coloumn"> </div>
+		<div class="fitur-coloumn">
+			<div class="fitur-icon">
+			</div>
+			<div class="fitur-text">
+				<h4>Live Streaming</h4>
+				<p>Dengan melihat video pernikahan secara langsung tamu undangan yang tidak dapat menghadiri acara dapat mengetahui suasana acara</p>
+			</div>
+		</div>
+		<div class="fitur-coloumn">
+		</div>
+		<div class="fitur-coloumn">
+		</div>
 
-		<div class="fitur-coloumn"> </div>
-		<div class="fitur-coloumn"> </div>
-		<div class="fitur-coloumn"> </div>
+		<div class="fitur-coloumn">
+		</div>
+		<div class="fitur-coloumn">
+		</div>
+		<div class="fitur-coloumn">
+		</div>
 
-		<div class="fitur-coloumn"> </div>
-		<div class="fitur-coloumn"> </div>
-		<div class="fitur-coloumn"> </div>
+		<div class="fitur-coloumn">
+		</div>
+		<div class="fitur-coloumn">
+		</div>
+		<div class="fitur-coloumn">
+		</div>
 
-		<div class="fitur-coloumn"> </div>
-		<div class="fitur-coloumn"> </div>
-		<div class="fitur-coloumn"> </div>
+		<div class="fitur-coloumn">
+		</div>
+		<div class="fitur-coloumn">
+		</div>
+		<div class="fitur-coloumn">
+		</div>
 </ul>

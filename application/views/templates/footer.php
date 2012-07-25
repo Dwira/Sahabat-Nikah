c59ec92ec95835d226ac74a4e3875091c6fb0ddd
dwira <dwirakwidodo@gmail.com>
2012-07-25 15:34:19 +0700

    edit posisi footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,4 +1,5 @@
 <div id="footer">
+	<br></br>
 	<ul>
 		<li><a href="<?= base_url() ?>home">Home</a></li>
 		<li><a href="<?= base_url() ?>about">About</a></li>
@@ -10,6 +11,7 @@
 		<li><a href="<?= base_url() ?>contact">Contact Us</a></li>
 		<li id="copyright">&copy 2012 Sahabatnikah</li>
 	</ul>
+	<br></br>
 </div>
 <script type="text/javascript" src="<?=base_url();?>theme/script/jquery.js"></script>
 <script type="text/javascript" src="<?=base_url();?>theme/script/jquery-effect.js"></script>

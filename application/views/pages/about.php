ce2b2184c96a903899f2dbb2a156f123ca3d18a3
Fatih Kalifa <fatih@pveyes.com>
2012-07-26 10:15:34 +0700

    Fix About

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -1,20 +1,33 @@
 <div id="about-content">
 	<div id="left-content">
-	<h1>About Us </h1>
+		<h1>About Us </h1>
+		<img src="<?=base_url();?>theme/img/about.jpg" />
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 	</div>
 	<div id="right-content">
-	<h1> Testimonial</h1>
-	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla cursus arcu eu quam volutpat viverra gravida sem venenatis. In quis orci turpis.</p><span> Fifi, Bandung </span>
-	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla cursus arcu eu quam volutpat viverra gravida sem venenatis. In quis orci turpis.</p><span> Fifi, Bandung </span>
-	<a href=# > <u>see more testimonial</u> </a>
-		<div id="right-content1">
-		<h2> Contact Us </h2>
-		<dl class="img-indent-bot">
-		<dd><span>Telephone:</span><strong>+62 xxx xxx xxx</strong></dd>
-		<dd><span>Email:</span><strong>sahabatnikah@gmail.com</strong></dd>
-		<dd><span>Facebook:</span><strong>Sahabatnikah.com</strong></dd>
-		<dd><span>Twitter:</span><strong>@Sahabatnikah</strong></dd>
-		</dl>
+		<div id="testimonial">
+			<h1> Testimonial</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla cursus arcu eu quam volutpat viverra gravida sem venenatis. In quis orci turpis.</p><span> Fifi, Bandung </span>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla cursus arcu eu quam volutpat viverra gravida sem venenatis. In quis orci turpis.</p><span> Fifi, Bandung </span>
+			<a href=# > <u>see more testimonial</u> </a>
+		</div>
+		<div id="contact-details">
+			<h2> Contact Us </h2>
+			<span>Telephone:</span><strong>+62 xxx xxx xxx</strong><br />
+			<span>Email:</span><strong>sahabatnikah@gmail.com</strong><br />
+			<span>Facebook:</span><strong>Sahabatnikah.com</strong><br />
+			<span>Twitter:</span><strong>@Sahabatnikah</strong><br />
 		</div>
 	</div>
 	<div class="clearfix"></div>

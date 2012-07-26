bba653e2aa5705a4b2e2560707418a3779427442
Fatih Kalifa <fatih@pveyes.com>
2012-07-26 11:14:36 +0700

    Fix Pricing
    Beta

--- application/views/pages/pricing.php
+++ application/views/pages/pricing.php
@@ -1,51 +1,79 @@
 <div id="pricing-content">
-	<div class="order-coloumn">
-		<h1>Paket 1</h1>
-		<table>
-		<tr>
-			<td><b>Lama Akses</b></td>
-			<td><dd></dd></td>
-			<td>28 hari</td>
-		</tr>
-		<tr>
-			<td><b>Jumlah Penonton</b></td>
-			<td></td>
-			<td>50 orang</td>
-		</tr>
-		</table>
-		<div class="button-order" ><a href="#"><img src="<?=base_url();?>theme/icon/order-button.jpg" height ="70px" width="141px"/></a></div>
+	<div id="package-list">
 	</div>
-	<div class="order-coloumn">
-		<h1>Paket 2</h1>
-		<table>
-		<tr>
-			<td><b>Lama Akses</b></td>
-			<td><dd></dd></td>
-			<td>21 hari</td>
-		</tr>
-		<tr>
-			<td><b>Jumlah Penonton</b></td>
-			<td></td>
-			<td>40 orang</td>
-		</tr>
-		</table>
-		<div class="button-order" ><a href="#"><img src="<?=base_url();?>theme/icon/order-button.jpg" height ="70px" width="141px"/></a></div>
+	<div id="comparation">
+		<div id="feature-list">
+			<ul class="comp-list feature">
+				<li>Lama Akses</li>
+				<li>Durasi Video</li>
+				<li>Jumlah Penonton</li>
+				<li>Custom Wedding Page</li>
+				<li>Guestbook</li>
+				<li>Harga</li>
+			</ul>
+		</div>
+		<div id="feature-status">
+			<div id="feature-status-free" class="feature-status">
+			<ul class="comp-list">
+				<li>1 hari</li>
+				<li>5 menit</li>
+				<li>5</li>
+				<li>Tidak</li>
+				<li>Tidak</li>
+				<li>Free</li>
+			</ul>
+			</div>
+			<div id="feature-status-bronze" class="feature-status">
+			<ul class="comp-list">
+				<li>4 hari</li>
+				<li>30 menit</li>
+				<li>30</li>
+				<li>Tidak</li>
+				<li>Ya</li>
+				<li>Rp 100.000</li>
+			</ul>
+			</div>
+			<div id="feature-status-silver" class="feature-status">
+			<ul class="comp-list">
+				<li>1 bulan</li>
+				<li>1 jam</li>
+				<li>100</li>
+				<li>Ya</li>
+				<li>Ya</li>
+				<li>Rp 200.000</li>
+			</ul>
+			</div>
+			<div id="feature-status-gold" class="feature-status">
+			<ul class="comp-list">
+				<li>2 bulan</li>
+				<li>2 jam</li>
+				<li>Unlimited</li>
+				<li>Ya</li>
+				<li>Ya</li>
+				<li>Rp 350.000</li>
+			</ul>
+			</div>
+			<div id="feature-status-platinum" class="feature-status">
+			<ul class="comp-list">
+				<li>Unlimited</li>
+				<li>Unlimited</li>
+				<li>Unlimited</li>
+				<li>Ya</li>
+				<li>Ya</li>
+				<li>Rp 500.000</li>
+			</ul>
+			</div>
+
+		</div>
 	</div>
-	<div class="order-coloumn">
-		<h1>Paket 3</h1>
-		<table>
-		<tr>
-			<td><b>Lama Akses</b></td>
-			<td><dd></dd></td>
-			<td>14 hari</td>
-		</tr>
-		<tr>
-			<td><b>Jumlah Penonton</b></td>
-			<td></td>
-			<td>30 orang</td>
-		</tr>
-		</table>
-		<div class="button-order" ><a href="#"><img src="<?=base_url();?>theme/icon/order-button.jpg" height ="70px" width="141px"/></a></div>
+	<div id="order-action">
+		<ul id="order-list">
+			<li><a href="#">SIGN UP</a></li>
+			<li><a href="#">SIGN UP</a></li>
+			<li><a href="#">SIGN UP</a></li>
+			<li><a href="#">SIGN UP</a></li>
+			<li><a href="#">SIGN UP</a></li>
+		</ul>
 	</div>
 	<div class="clearfix"></div>
 </div>

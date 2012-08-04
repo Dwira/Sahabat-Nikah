28df1f48939601a5419be03259f8b3dec3a25be8
Nurul Magfirotunnisak <nmfifi@gmail.com>
2012-08-04 14:07:37 +0700

    gallery..

--- application/views/pages/gallery.php
+++ application/views/pages/gallery.php
@@ -15,6 +15,11 @@
 					</div>
 					<div class="shadow"></div>
 				</div>
+				<div class="info">
+					<h1>Gunawan Sasongko & Rizki Amalia</h1>
+					<h2>Bandung, 12 Desember 2012</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</br></br><i>Source: IMDB</i></p>
+				</div>
 			</li>
 			<li>
 				<div class="bluraywrap">
@@ -22,6 +27,8 @@
 					<div class="photo-coloumn"> </div>
 					</div>
 					<div class="shadow"></div>
+				</div>
+				<div class="info">
 				</div>
 			</li>
 			<li>
@@ -31,6 +38,8 @@
 					</div>
 					<div class="shadow"></div>
 				</div>
+				<div class="info">
+				</div>
 			</li>
 			<li>
 				<div class="bluraywrap">
@@ -38,6 +47,8 @@
 					<div class="photo-coloumn"> </div>
 					</div>
 					<div class="shadow"></div>
+				</div>
+				<div class="info">
 				</div>
 			</li>
 			</ul>
@@ -48,6 +59,8 @@
 					<div class="photo-coloumn"> </div>
 					</div>
 					<div class="shadow"></div>
+				</div>
+				<div class="info">
 				</div>
 			</li>
 			<li>
@@ -103,7 +116,10 @@
 					<div class="shadow"></div>
 				</div>
 				<div class="info">
-
+				<h1>Gunawan Sasongko & Rizki Amalia</h1>
+				<h2>Bandung, 12 Desember 2012</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</br></br><i>Source: IMDB</i></p>
+				</div>
 			</li>
 			<li>
 				<div class="bluraywrap">
